5ab34db3a52bfc12f2157eb778d677c955ff3d02
Just Barcodes <just-barcodes@users.noreply.github.com>
2026-05-24 18:45:56 +0200

    feat(hypr): disable splash on desktop

--- dot_config/hypr/style.lua
+++ dot_config/hypr/style.lua
@@ -47,8 +47,9 @@ hl.config({
     },
 
     misc = {
-        force_default_wallpaper = 0,
-        disable_hyprland_logo   = true,
+        force_default_wallpaper  = 0,
+        disable_hyprland_logo    = true,
+        disable_splash_rendering = true,
     },
 
     -- Selenized Dark palette:
